743ce4588270aa6f28c3aad09f17a5c637898401
Harish <harish@raitonsemi.com>
2026-08-14 10:54:05 +0530

    Delete tbold

--- tbold/ips_core/axi_intc/intc_config_obj.sv
+++
@@ -1,8 +0,0 @@
-class intc_config_obj extends uvm_object();
-    `uvm_object_utils(intc_config_obj)
-    `NEW_OBJ
-
-    uvm_active_passive_enum lite_intc_active;
-    uvm_active_passive_enum intr_active;
-
-endclass : intc_config_obj
--- tbold/ips_core/axi_intc/intr_in_agent.sv
+++
@@ -1,8 +0,0 @@
-class intr_in_agent extends uvm_agent;
-    `uvm_component_utils(intr_in_agent)
-    `NEW_COMP
-    
-    function void build();
-    endfunction
-
-endclass
