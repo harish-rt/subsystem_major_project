a46c69c54baa2e282a343da41c039789da810c7a
Harish <harish@raitonsemi.com>
2026-08-14 10:54:05 +0530

    Delete tbold

--- tbold/ips_core/axi_intc/irq_out_agent.sv
+++
@@ -1,8 +0,0 @@
-class irq_out_agent extends uvm_agent;
-    `uvm_component_utils(irq_out_agent)
-    `NEW_COMP
-
-    function void build();
-    endfunction
-
-endclass
--- tbold/ips_core/axi_intc/lite_intc_monitor.sv
+++
@@ -1,5 +0,0 @@
-class lite_intc_monitor extends uvm_monitor;
-    `uvm_component_utils(lite_intc_monitor)
-    `NEW_COMP
-    
-endclass
