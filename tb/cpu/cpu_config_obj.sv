883f34949e414baee768595bc41f2214dc55aa19
Harish <harish@raitonsemi.com>
2026-08-24 16:55:31 +0530

    Update cpu_config_obj.sv

--- tb/cpu/cpu_config_obj.sv
+++ tb/cpu/cpu_config_obj.sv
@@ -5,6 +5,7 @@ class config_obj extends uvm_object();
    virtual axi4_lite_intf    cpu_i;
    //virtual cpu_intf    cpu_i;
    bit mas_is_active; 
+   //riscv_is_active in top
 
   function new (string name = "config_obj");
      super.new (name);
