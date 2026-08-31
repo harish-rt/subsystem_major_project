44bd2304b0d3c0d78580429f2e9c8c594a408943
Mahesh <maheshkumbar@raitonsemi.com>
2026-08-31 17:14:15 +0530

    added_sanity_check_for_memory

--- tb/top/base_test.sv
+++ tb/top/base_test.sv
@@ -131,15 +131,37 @@ class config_intc_test extends cpu_base_test;
     endtask
 endclass : config_intc_test
 
+class mem_wr_rd_test extends cpu_base_test;
+    `uvm_component_utils(mem_wr_rd_test)
+    `NEW_COMP
+
+    read_mem_seq mem_seq;
+    load_mem_seq seq;
+
+    task main_phase(uvm_phase phase);
+        seq     = load_mem_seq ::type_id::create("seq");
+        mem_seq = read_mem_seq :: type_id :: create("mem_seq");
+        phase.raise_objection(this);
+            seq.start(w_env.c_env.cpu_agt.sqr);
+            mem_seq.start(w_env.c_env.cpu_agt.sqr);
+            phase.phase_done.set_drain_time(this, 100ns);
+            phase.drop_objection(this);
+    endtask
+endclass : mem_wr_rd_test
+ 
+
 class cdma_wr_rd_test extends cpu_base_test;
     `uvm_component_utils(cdma_wr_rd_test)
     `NEW_COMP
 
+    load_mem_seq mem_seq;
     cdma_read_write_seq seq;
 
     task main_phase(uvm_phase phase);
-        seq = cdma_read_write_seq::type_id::create("seq");
+        seq     = cdma_read_write_seq::type_id::create("seq");
+        mem_seq = load_mem_seq :: type_id :: create("mem_seq");
         phase.raise_objection(this);
+            mem_seq.start(w_env.c_env.cpu_agt.sqr);
             seq.start(w_env.c_env.cpu_agt.sqr);
             phase.phase_done.set_drain_time(this, 100ns);
         phase.drop_objection(this);
