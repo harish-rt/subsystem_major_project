1d6198fef7734675ed71bdde732a594ed163810f
Mahesh <maheshkumbar@raitonsemi.com>
2026-08-31 17:14:15 +0530

    added_sanity_check_for_memory

--- tb/cpu/cpu_sequence.sv
+++ tb/cpu/cpu_sequence.sv
@@ -265,6 +265,66 @@ class config_intc_seq extends base_cpu_sequence;
 endclass : config_intc_seq
 */
 
+class load_mem_seq extends base_cpu_sequence;
+    `uvm_object_utils(load_mem_seq)
+    `NEW_OBJ
+    
+    int addr;
+
+    task body();
+        super.body();
+    
+        `uvm_info("load_mem_seq", "start of Memory load Sequence", UVM_MEDIUM)
+
+        for (int i = 0; i < 20; i++) begin        
+            addr = LITE_MEM_BASE + (i*4);
+            start_item(pkt);
+            if(!pkt.randomize() with {
+                AWADDR  == addr;
+                WSTRB   == 'hf; 
+                operation   == WRITE;
+                })begin
+                `uvm_error(get_full_name(), "randomization_failed")
+            end
+            finish_item(pkt);
+            get_response(pkt);             
+        end
+        
+        `uvm_info("load_mem_seq", "end of Memory load Sequence", UVM_MEDIUM)
+
+     endtask
+endclass:load_mem_seq
+
+class read_mem_seq extends base_cpu_sequence;
+    `uvm_object_utils(read_mem_seq)
+    `NEW_OBJ
+    
+    int addr;
+
+    task body();
+        super.body();
+    
+        `uvm_info("read_mem_seq", "start of Memory read Sequence", UVM_MEDIUM)
+
+        for (int i = 0; i < 20; i++) begin        
+            addr = LITE_MEM_BASE + (i*4);
+            start_item(pkt);
+            if(!pkt.randomize() with {
+                ARADDR  == addr;
+                operation   == READ;
+                })begin
+                `uvm_error(get_full_name(), "randomization_failed")
+            end
+            finish_item(pkt);
+            get_response(pkt);             
+        end
+        
+        `uvm_info("read_mem_seq", "end of Memory read Sequence", UVM_MEDIUM)
+
+     endtask
+endclass:read_mem_seq
+
+
 class cdma_read_write_seq extends base_cpu_sequence;
     `uvm_object_utils(cdma_read_write_seq)
     `NEW_OBJ
@@ -275,21 +335,6 @@ class cdma_read_write_seq extends base_cpu_sequence;
         super.body();
     
         `uvm_info("cdma_rd_wr_seq", "start of Config CDMA Sequence", UVM_MEDIUM)
-
-        for (int i = 0; i < 10; i++) begin        
-            addr = 32'h8000_0000 + (i*4);
-            start_item(pkt);
-            if(!pkt.randomize() with {
-                AWADDR  == addr;
-                //WSTRB   == {(DATA_WIDTH/8){1'b1}};
-                WSTRB   == 'hf; 
-                operation   == WRITE;
-                })begin
-                `uvm_error(get_full_name(), "randomization_failed")
-            end
-            finish_item(pkt);
-            get_response(pkt);             
-        end
 
     do begin
         start_item(pkt);
@@ -358,4 +403,5 @@ class isr_seq extends base_cpu_sequence;
         super.body();
     endtask
 endclass : isr_seq
+
 
